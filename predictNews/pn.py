3b57125ef4afd562c15645c24024c9cdb718a807
zexon7 <zexon01@gmail.com>
2020-05-16 22:28:51 +0800

    update seg.ipynb, tn.py and pn.py

--- predictNews/pn.py
+++ predictNews/pn.py
@@ -1,13 +1,32 @@
 import fasttext
+import jieba
 
 #trained data path
 path = ".\\model\\model_news.bin"
 
 model = fasttext.load_model(path)
 
-#check model info
-print(model.words)
-print(model.labels)
+def print_results(N, p, r):
+    print("N\t" + str(N))
+    print("P@{}\t{:.3f}".format(1, p))
+    print("R@{}\t{:.3f}".format(1, r))
 
-print(model.predict("全球人類吃素"))
-#雞眼用香蕉皮就可以治好
+print_results(*model.test(".\\segdata\\segData.txt"))
+
+#check model info
+#print(model.words)
+#print(model.labels)
+
+#__label__fake	影片宣稱「人和猴子交配生子」？
+#__label__partialError	「吃冷的米飯能抗腸癌」？
+n1 = "人猴子交配生子"
+n2 = "吃冷米飯抗腸癌"
+
+s1 = jieba.cut(n1, cut_all=False)
+d1 = " ".join(s1)
+s2 = jieba.cut(n2, cut_all=False)
+d2 = " ".join(s2)
+
+#prdict result
+print(model.predict(d1))
+print(model.predict(d2))
--- predictNews/tn.py
+++ predictNews/tn.py
@@ -1,7 +1,15 @@
 import fasttext
+import pandas as pd
 
 #data path
-path = ".\\segdata\\segData.txt"
+path = ".\\segdata\\segData.csv"
+
+#data = pd.read_csv(path, encoding='UTF-8')
+#data.iloc[0:int(len(data)*0.8)].to_csv('.\\segdata\\train.txt', header=None, index=None, encoding='utf-8-sig', mode='w')
+#data.iloc[int(len(data)*0.8):int(len(data)*0.9)].to_csv('.\\segdata\\test.txt', header=None, index=None, encoding='utf-8-sig', mode='w')
+
+#train_data = ".\\segdata\\train.txt"
+#test_data = ".\\segdata\\test.txt"
 
 #train
 model = fasttext.train_supervised(path)
@@ -10,19 +18,3 @@ model = fasttext.train_supervised(path)
 
 #save model
 model.save_model(".\\model\\model_news.bin")
-
-'''
-#check the model effectiveness
-def print_results(N, p, r):
-    print("N\t" + str(N))
-    print("P@{}\t{:.3f}".format(1, p))
-    print("R@{}\t{:.3f}".format(1, r))
-
-print_results(*model.test(path))
-'''
-def print_results(N, p, r):
-    print("N\t" + str(N))
-    print("P@{}\t{:.3f}".format(1, p))
-    print("R@{}\t{:.3f}".format(1, r))
-
-print_results(*model.test(path))
